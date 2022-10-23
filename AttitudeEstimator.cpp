840177a9aa49ad6b2e9009180836ca48049b0b45
Michael Barbosa <michaelfeliphe@gmail.com>
2022-10-23 09:58:35 -0300

    Adicionar arquivos de projeto.

---
+++ AttitudeEstimator.cpp
@@ -0,0 +1,12 @@
+﻿// AttitudeEstimator.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "AttitudeEstimator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
